2931f266724a97ba30739025096b74da37374354
SA926-COBRA <sayanacar926@gmail.com>
2025-03-04 21:43:30 +0545

    feat: add AverageOfThreeNumbers program to calculate the average of three integers

--- part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -8,5 +8,18 @@ public class AverageOfThreeNumbers {
 
         // Write your program here
 
+        System.out.println("Give the first number: ");
+        int a = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number: ");
+        int b = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number: ");
+        int c = Integer.valueOf(scanner.nextLine());
+
+        double avg = 1.0 * (a + b + c) / 3;
+
+        System.out.println("The average is " + avg);
+        scanner.close();
     }
 }
